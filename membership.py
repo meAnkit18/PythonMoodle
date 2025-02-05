13caf88d5caf82cf1ee0b45f75f5cf431d4a6c22
Ankit Kumar <ankit1842kumar@gmail.com>
2025-02-05 15:31:43 +0530

    Moodle questions solution

---
+++ membership.py
@@ -0,0 +1,5 @@
+lst = [1, 2, 3, 4, 5]
+print(3 in lst, 10 not in lst)
+
+txt = "Hello Python"
+print("Python" in txt, "Java" not in txt)
---
+++ sumofdigit.py
@@ -0,0 +1,5 @@
+num = 1234 
+
+sum_of_digits = (num // 1000) + (num // 100 % 10) + (num // 10 % 10) + (num % 10)
+
+print("Sum of digits:", sum_of_digits)
